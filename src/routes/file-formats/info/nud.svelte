987fab8cadc4cb3920c2c7f6decc161bcc066bdf
Thea Schöbl <wulkanat@gmail.com>
2022-04-20 00:51:29 +0200

    Update nud info

--- src/routes/file-formats/info/nud.svelte
+++ src/routes/file-formats/info/nud.svelte
@@ -3,7 +3,26 @@
 </script>
 
 <table>
-  <caption>Header</caption>
+  <caption>.NUD Model</caption>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#header">Header</a><br />⥥</td>
+  </tr>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#polygons">Polygons</a><br />⥥</td>
+  </tr>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#indices">Indices</a><br />⥥</td>
+  </tr>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#vertices">Vertices</a><br />⥥</td>
+  </tr>
+  <tr>
+    <td colspan="16">name</td>
+  </tr>
+</table>
+
+<table>
+  <caption id="header">Header</caption>
   <tr>
     <td colspan="4">Signature (NDWD)</td>
     <td colspan="4">u32 <var>totalLength</var></td>
@@ -17,44 +36,30 @@
     <td colspan="4">u32 <var>lengthSection3</var></td>
     <td colspan="4" />
   </tr>
-  <tr>
-    <td style="height: 128px" colspan="16">⇑<br />Polygons<br />⥥</td>
-  </tr>
-  <tr>
-    <td colspan="16">0</td>
-  </tr>
-  <tr>
-    <td style="height: 128px" colspan="16">⇑<br />Indices<br />⥥</td>
-  </tr>
-  <tr>
-    <td style="height: 128px" colspan="16">⇑<br />Vertices<br />⥥</td>
-  </tr>
-  <tr>
-    <td colspan="16">name</td>
-  </tr>
+  <tr><td colspan="16">?</td> </tr>
 </table>
 
 <table>
-  <caption>Polygons</caption>
+  <caption id="polygons">Polygons</caption>
   <tr>
-    <td style="height: 96px" colspan="16">?</td>
+    <td style="height: 64px" colspan="16">?</td>
   </tr>
   <tr>
     <td colspan="8" />
-    <td colspan="2">FF FF</td>
+    <td colspan="2">0xFFFF</td>
     <td colspan="2">u16 <var>polyCount</var></td>
     <td colspan="4">u32 <var>polyDataLength</var></td>
   </tr>
   <tr>
-    <td style="height: 128px" colspan="16">⇑<br />Polygon Info<br />⥥</td>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#polygon-info">Polygon Info</a><br />⥥</td>
   </tr>
   <tr>
-    <td style="height: 128px" colspan="16">⇑<br />Polygon Data<br />⥥</td>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#polygon-data">Polygon Data</a><br />⥥</td>
   </tr>
 </table>
 
 <table>
-  <caption>Polygon Info</caption>
+  <caption id="polygon-info">Polygon Info</caption>
   <tr>
     <td colspan="4">u32 <var>indexOffset</var></td>
     <td colspan="4">u32 <var>vertexOffset</var></td>
@@ -63,7 +68,7 @@
     <td colspan="2">u16 <var>?</var></td>
   </tr>
   <tr>
-    <td colspan="4">u32 <var>polyDataStart</var></td>
+    <td colspan="4">u32 <var>polyDataStart?</var></td>
     <td colspan="12" />
   </tr>
   <tr>
@@ -72,12 +77,48 @@
   </tr>
 </table>
 
-<p>Indices are an array of u16s, which form n-gons that are closed up by an index of <code>0xffff</code>.</p>
+<table>
+  <caption id="polygon-data">Polygon Data</caption>
+  <tr>
+    <td style="height: 160px" colspan="16">?</td>
+  </tr>
+  <tr><td colspan="16">0</td></tr>
+</table>
 
-<p>
-  Vertices are an array of u32s, three for the position, three for the uvs (?), and then closed off by a high
-  u32 or multiple smaller numbers of unknown purpose.
-</p>
+<table>
+  <caption id="n-gon">N-Gon</caption>
+  <tr>
+    <td colspan="14">⟸ u16 ⟹</td>
+    <td colspan="2">0xFFFF</td>
+  </tr>
+</table>
+
+<table>
+  <caption id="indices">Indices</caption>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#n-gon">N-Gons</a><br />⥥</td>
+  </tr>
+</table>
+
+<table>
+  <caption id="vertex">Vertex</caption>
+  <tr>
+    <td colspan="4">f32 <var>posX</var></td>
+    <td colspan="4">f32 <var>posY</var></td>
+    <td colspan="4">f32 <var>posZ</var></td>
+    <td colspan="4">f32 <var>?</var></td>
+    <td colspan="4">f32 <var>?</var></td>
+    <td colspan="4">f32 <var>?</var></td>
+    <td colspan="4">u32 <var>?</var></td>
+  </tr>
+</table>
+
+<table>
+  <caption id="vertices">Vertices</caption>
+  <tr>
+    <td style="height: 128px" colspan="16">⇑<br /><a href="#vertex">vertices</a><br />⥥</td>
+  </tr>
+</table>
 
 <style lang="scss">
   @import "../../../lib/style/theme.scss";
