57b4878f28264c5c8c72bfdb4ba67462aae4da00
Thea Schöbl <dev@theaninova.de>
2022-05-16 19:54:16 +0200

    Add backdrop animations

--- src/lib/components/Backdrop.svelte
+++ src/lib/components/Backdrop.svelte
@@ -1,4 +1,6 @@
 <div class="content">
+  <div class="backdrop" />
+
   <div class="backdrop-rects" style="height: 120px; background-color:  rgba(0,0,0,0.3)" />
   <div class="backdrop-rects" style="height: 30px; background-color:  rgba(0,0,0,0.3)" />
 
@@ -18,28 +20,25 @@
 
   @keyframes rects {
     from {
-      transform: translateX(-100%) scaleX(400%) translateY(-100%);
+      transform: translateX(-25%) translateY(-100%);
     }
     to {
-      transform: translateX(100%) scaleX(400%) translateY(-100%);
+      transform: translateX(25%) translateY(-100%);
+    }
+  }
+
+  @keyframes background {
+    from {
+      transform: translateX(-25%);
+    }
+    to {
+      transform: translateX(25%);
     }
   }
 
   .content {
     grid-row: 1 / span 2;
     grid-column: 1;
-
-    background-image: linear-gradient(
-        to bottom,
-        black,
-        #13131377,
-        #131313dd 30%,
-        #131313dd 70%,
-        #13131377,
-        black
-      ),
-      url($grid);
-    background-size: 30px;
 
     display: grid;
     align-items: center;
@@ -56,6 +55,25 @@
 
   .backdrop-rects {
     width: 120%;
+    z-index: 5;
+  }
+
+  .backdrop {
+    width: 200%;
+    height: 100%;
+    background-image: linear-gradient(
+        to bottom,
+        black,
+        #13131377,
+        #131313dd 30%,
+        #131313dd 70%,
+        #13131377,
+        black
+      ),
+      url($grid);
+    background-size: 30px;
+
+    animation: background 24s linear infinite;
   }
 
   .flowing-rects {
@@ -63,7 +81,7 @@
     flex-direction: row;
     justify-content: space-around;
 
-    width: 100%;
+    width: 200%;
 
     animation: rects 6s linear infinite;
   }
@@ -71,6 +89,9 @@
   @media (min-width: 768px) {
     .flowing-rects {
       animation-duration: 12s;
+    }
+    .backdrop {
+      animation-duration: 96s;
     }
   }
 
@@ -87,7 +108,7 @@
   }
 
   .rect {
-    width: clamp(20px, 8vw, 200px);
+    width: clamp(40px, 32vw, 400px);
     height: 80px;
 
     background-color: black;
@@ -96,6 +117,6 @@
   }
 
   .last-rect {
-    margin-inline: 40px;
+    margin-inline: 10%;
   }
 </style>
