03152110a34b9670f7cd7e93a1f742d6cabd6b16
Thea Schöbl <dev@theaninova.de>
2022-05-16 18:12:37 +0200

    Improve router animations

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -5,13 +5,15 @@
 </script>
 
 {#key activeRoute}
-  <main in:fly={{duration: 200, delay: 300, x: -150}} out:fly={{duration: 200, x: -150}}>
+  <main in:fly={{duration: 250, delay: 300, x: -150}} out:fly={{duration: 250, x: -150}}>
     <slot />
   </main>
 {/key}
 
 <style global>
   main {
-    will-change: transform;
+    grid-row: 2;
+    grid-column: 1;
+    will-change: transform, opacity;
   }
 </style>
